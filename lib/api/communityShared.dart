42c13c844f4b7b24ce0f500d1430c559a30e71b7
kainem17 <123512339+kainem17@users.noreply.github.com>
2024-10-31 19:40:20 -0400

    Adding shared entries for multiple communities

--- lib/api/communityShared.dart
+++ lib/api/communityShared.dart
@@ -20,15 +20,34 @@ class CommunityShared {
         "userLocation": updatedLocation.data,
       };
 
-      final collectionRef =
+
+      // Find id by community name
+      QuerySnapshot communitySnapshot = await FirebaseFirestore.instance
+        .collection("CommunitiesDemo")
+        .where("communityName", isEqualTo: communityName)
+        .get();
+
+      // make sure community exists
+      if (communitySnapshot.docs.isNotEmpty) {
+        String communityId = communitySnapshot.docs.first.id;
+
+        // use id to add to shared entries
+        final collectionRef =
           FirebaseFirestore.instance.collection("CommunitiesDemo")
-            .doc(communityName)
+            .doc(communityId)
             .collection("Virtues")
             .doc(virtueUsed)
             .collection("sharedEntries");
-      await collectionRef.add(sharedEntry);
+        await collectionRef.add(sharedEntry);
 
-      return {"Success": true, "response": "Submitted in shared database"};
+        return {"Success": true, "response": "Submitted in shared database"};
+      } 
+      else {
+        // community wasnt found?
+        print('Couldnt find community to add virtue entry to');
+        return {'Success': false, 'Error': 'Couldnt find community'};
+      }
+
     } on FirebaseException catch (error) {
       return {'Success': false, 'Error': error.message};
     } catch (error) {
