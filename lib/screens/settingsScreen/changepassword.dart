fcc1cbf672406302d5a890c45f776acd20aadc1e
kainem17 <123512339+kainem17@users.noreply.github.com>
2024-11-01 21:45:56 -0400

    Cleaned up change passwod

--- lib/screens/settingsScreen/changepassword.dart
+++ lib/screens/settingsScreen/changepassword.dart
@@ -7,6 +7,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:virtuetracker/App_Configuration/appColors.dart';
 import 'package:virtuetracker/Models/UserInfoModel.dart';
 import 'package:virtuetracker/controllers/settingsController.dart';
+import 'package:virtuetracker/screens/settingsScreen/editprofile.dart';
 import 'package:virtuetracker/widgets/reauthenticateShowDialogWidget.dart';
 import 'package:virtuetracker/widgets/toastNotificationWidget.dart';
 
@@ -126,20 +127,23 @@ class _ChangePasswordPageState extends State<ChangePasswordPage> {
                             color: Colors.white,
                             borderRadius: BorderRadius.circular(5.0),
                           ),
-                          child: TextField(
+                          child: TextFormField(
                             controller: newPassword,
+                            obscureText: true,
                             onChanged: (newValue) {
                               setState(() {});
                             },
                             decoration: InputDecoration(
                               contentPadding: EdgeInsets.zero,
                               isDense: true,
-                              hintText: 'Eg. abc@gmail.com',
+                              hintText: 'Enter new password',
                               hintStyle: GoogleFonts.tinos(
-                                  textStyle: TextStyle(color: Colors.black)),
+                                  textStyle: TextStyle(color: Color.fromARGB(255, 167, 166, 166))),
                               border:
                                   InputBorder.none, // Hide the default border
                             ),
+                            validator: validatePassword,
+                            autovalidateMode: AutovalidateMode.onUserInteraction,
                           ),
                         ),
                         SizedBox(
@@ -165,20 +169,23 @@ class _ChangePasswordPageState extends State<ChangePasswordPage> {
                             color: Colors.white,
                             borderRadius: BorderRadius.circular(5.0),
                           ),
-                          child: TextField(
+                          child: TextFormField(
                             controller: confirmPassword,
+                            obscureText: true,
                             onChanged: (newValue) {
                               setState(() {});
                             },
                             decoration: InputDecoration(
                               contentPadding: EdgeInsets.zero,
                               isDense: true,
-                              hintText: 'Eg. john doe',
+                              hintText: 'Confirm new password',
                               hintStyle: GoogleFonts.tinos(
-                                  textStyle: TextStyle(color: Colors.black)),
+                                  textStyle: TextStyle(color: Color.fromARGB(255, 167, 166, 166))),
                               border:
                                   InputBorder.none, // Hide the default border
                             ),
+                            validator: validatePassword,
+                            autovalidateMode: AutovalidateMode.onUserInteraction,
                           ),
                         ),
                       ],
