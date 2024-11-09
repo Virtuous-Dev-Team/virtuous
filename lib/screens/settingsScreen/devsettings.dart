e93589e2977cab5d9f8e72c342b8c96aeb6a052c
halicia427 <h.alicia427@gmail.com>
2024-11-09 00:15:14 -0500

    Began Dev Page Layout

--- lib/screens/settingsScreen/devsettings.dart
+++ lib/screens/settingsScreen/devsettings.dart
@@ -28,6 +28,12 @@ class DevSettingsPage extends ConsumerStatefulWidget {
 }
 
 class _DevSettingsPageState extends ConsumerState<DevSettingsPage> {
+  TextEditingController newProfileName = TextEditingController();
+  TextEditingController newEmail = TextEditingController();
+  TextEditingController newCareer = TextEditingController(); //newRole
+  TextEditingController newCareerLength = TextEditingController(); //newLength
+  late String currentCommunity;
+  bool newListExist = false;
   @override
   void initState() {
     super.initState();
@@ -42,75 +48,133 @@ class _DevSettingsPageState extends ConsumerState<DevSettingsPage> {
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
+
+    List<String> careerDropdownValues = [
+        'Legal',
+        'Alcoholics Anonymous',
+    ];
+
     return Scaffold(
-        backgroundColor: Color(0xFFEFE5CC),
-        appBar: AppBarWidget('regular'),
-        body: Center(
-          child: SingleChildScrollView(
-            child: Container(
-              width: screenWidth,
-              height: screenHeight / 1.2,
-              decoration: BoxDecoration(
-                color: Color(0xFFFFFDF9),
-                border: Border.all(color: Color(0xFFFEFE5CC), width: 9.0),
-                borderRadius: const BorderRadius.all(
-                  Radius.circular(20),
-                ),
+    backgroundColor: Color(0xFFEFE5CC),
+    appBar: AppBarWidget('regular'),
+    body: SingleChildScrollView(
+      child: Container(
+        child: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Color(0xFFFFFDF9),
+              border: Border.all(color: Color(0xFFFEFE5CC), width: 9.0),
+              borderRadius: const BorderRadius.all(
+                Radius.circular(20),
               ),
-              padding: EdgeInsets.only(
-                top: screenHeight / 50,
-                bottom: screenHeight / 50,
-                left: screenWidth / 30,
-                right: screenWidth / 30,
-              ),
+            ),
+            padding: EdgeInsets.only(
+              top: screenHeight / 50,
+              bottom: screenHeight / 25,
+              left: screenWidth / 30,
+              right: screenWidth / 30,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Text(
-                    "Terms of Use",
-                    style: GoogleFonts.adamina(
-                      textStyle: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                      ),
+                    "Communities",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
                     ),
                   ),
                   SizedBox(
-                    height: screenHeight / 40,
+                    height: 30,
                   ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Last Modified on 02/14/2024',
-                        style: GoogleFonts.adamina(
-                          textStyle: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.normal,
-                              color: Colours.black),
+                  // Container(
+                  //       child:
+                  //        ListView.builder(
+                  //         itemCount: careerDropdownValues.length,
+                  //         itemBuilder: (context, index) {
+                  //           return MaterialButton(
+                  //               onPressed: () {
+                  //               },
+                  //               child: Container(
+                  //                 decoration: BoxDecoration(
+                  //                   color: Colours.swatch(
+                  //                       clrBackground), // Dark purple color
+                  //                   borderRadius: BorderRadius.circular(
+                  //                       5), // Adjusted border radius
+                  //                   boxShadow: [
+                  //                     BoxShadow(
+                  //                       color: Colors.grey.withOpacity(0.5),
+                  //                       spreadRadius: 2,
+                  //                       blurRadius: 4,
+                  //                       offset: Offset(0, 3),
+                  //                     ),
+                  //                   ],
+                  //                 ),
+                  //                 width: 210,
+                  //                 height: 50,
+                  //                 child: Center(
+                  //                   child: Text(
+                  //                     "Edit ${careerDropdownValues[index]} Community",
+                  //                     style: GoogleFonts.tinos(
+                  //                       textStyle: TextStyle(
+                  //                         fontSize: 20,
+                  //                         fontWeight: FontWeight.normal,
+                  //                       ),
+                  //                     ),
+                  //                   ),
+                  //                 ),
+                  //               ),
+                  //             );
+                  //         }
+                  //       ),
+                  // ),
+                  SizedBox(
+                      height: 25,
+                  ),
+                   Center(
+                    child: MaterialButton(
+                      onPressed: () {
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colours.swatch(
+                              clrBackground), // Dark purple color
+                          borderRadius: BorderRadius.circular(
+                              5), // Adjusted border radius
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 2,
+                              blurRadius: 4,
+                              offset: Offset(0, 3),
+                            ),
+                          ],
+                        ),
+                        width: 210,
+                        height: 50,
+                        child: Center(
+                          child: Text(
+                            "Add Community",
+                            style: GoogleFonts.tinos(
+                              textStyle: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.normal,
+                              ),
+                            ),
+                          ),
                         ),
                       ),
-                      SizedBox(
-                        height: screenHeight / 50,
-                      ),
-                      Text(
-                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Penatibus et magnis dis parturient montes nascetur ridiculus. Viverra orci sagittis eu volutpat odio facilisis. Tincidunt augue interdum velit euismod in. Aliquam eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis. Praesent tristique magna sit amet purus gravida quis. Aliquet porttitor lacus luctus accumsan tortor posuere. Lacus vel facilisis volutpat est velit. Magna eget est lorem ipsum dolor. Amet tellus cras adipiscing enim eu turpis egestas pretium aenean.',
-                        style: GoogleFonts.adamina(
-                          textStyle: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.normal,
-                              color: Colours.swatch(clrText)),
-                        ),
-                      ),
-                    ],
+                    ),
                   ),
                 ],
+                ),
               ),
             ),
           ),
-        ));
+        ),
+      ));
   }
 }
